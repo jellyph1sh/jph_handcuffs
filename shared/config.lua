f2e948e63336ff2b0a67a5e390e083e6aacbacf4
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 12:33:19 +0200

    :wrench: | Add config file.

---
+++ shared/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.AnimDict = "mp_arresting"
+Config.HandcuffAnim = "idle"
+Config.HandcuffsProp = "p_cs_cuffs_02_s"
+
+Config.DisableActions = {10, 11, 21, 22, 23, 24, 25, 36, 44, 47, 51, 52, 55, 56, 57, 58, 66, 67, 69, 70, 81, 82, 83, 84, 85, 91, 101, 144, 145, 257, 263, 264}
