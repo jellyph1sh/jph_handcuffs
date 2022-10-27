1b8efed58d533f5f1e71a366cb98442642a8f257
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 12:31:32 +0200

    :rocket: | Deploy resource manifest.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "Basic handcuffs script."
+author "Time_XP"
+
+client_script "client/cl_handcuffs.lua"
+
+server_script "server/sv_handcuffs.lua"
+
+shared_script "shared/config.lua"
